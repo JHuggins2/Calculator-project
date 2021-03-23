818ad60e4540f2f27f0e15e435c54c1f20224fe0
Jacob Huggins <hugginsjw@g.cofc.edu>
2021-03-23 12:05:46 -0400

    Finished App

--- app/src/main/java/com/example/calculator_huggins/MainActivity.java
+++ app/src/main/java/com/example/calculator_huggins/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calculator_huggins;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,16 +49,26 @@ public class MainActivity extends AppCompatActivity {
         switch(buttonText){
             case "CE":
                 formula = "";
-                System.out.println("-------------:" + formula + ":------------------");
                 updateDisplay();
                 break;
             case "C":
-                if(!formula.isEmpty()) formula = formula.substring(0,formula.length()-1);
-                System.out.println("-------------:" + formula + ":------------------");
+                if(!formula.isEmpty()){
+                    int lastIndex = 0;
+
+                    if(lastIndex < formula.lastIndexOf("+")) lastIndex = formula.lastIndexOf("+");
+                    if(lastIndex < formula.lastIndexOf("-")) lastIndex = formula.lastIndexOf("-");
+                    if(lastIndex < formula.lastIndexOf("*")) lastIndex = formula.lastIndexOf("*");
+                    if(lastIndex < formula.lastIndexOf("/")) lastIndex = formula.lastIndexOf("/");
+
+                    if(lastIndex == 0) formula = "";
+                    if(lastIndex == formula.length()-1) formula = formula.substring(0,formula.length()-1);
+                    else if(lastIndex > 0) formula = formula.substring(0,lastIndex+1);
+
+
+                }
                 updateDisplay();
                 break;
             case "=":
-                System.out.println("-------------" + formula + "------------------");
                 if(!formula.isEmpty()) pastFormulas.push(formula);
                 double formulaSolve = calculate(formula);
                 if(formulaSolve == (long) formulaSolve) formula = String.format("%d",(long)formulaSolve);
@@ -84,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
                             tempForm.insert(lastIndex + 1, "_");
                             formula = tempForm.toString();
                         }
-                        System.out.println("--------__:" + formula);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -93,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 formula += buttonText;
-                System.out.println("-------------" + formula + "------------------");
                 updateDisplay();
                 break;
         }
@@ -106,12 +116,12 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i < formula.length(); i++){
             char ch = formula.charAt(i);
 
-            if(Character.isDigit(ch) || ch == '_') tempNum += ch;
-            if((!Character.isDigit(ch) && ch != '_') || i == formula.length()-1){
-                int num = 0;
+            if(Character.isDigit(ch) || ch == '_' || ch == '.') tempNum += ch;
+            if((!Character.isDigit(ch) && ch != '_' && ch != '.') || i == formula.length()-1){
+                double num = 0;
                 try {
-                    if (tempNum.charAt(0) == '_') num = -1 * Integer.parseInt(tempNum.substring(1));
-                    else num = Integer.parseInt(tempNum);
+                    if (tempNum.charAt(0) == '_') num = -1 * Double.parseDouble(tempNum.substring(1));
+                    else num = Double.parseDouble(tempNum);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -137,5 +147,50 @@ public class MainActivity extends AppCompatActivity {
             else tempFormula += ch;
         }
         display.setText(tempFormula);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(getString(R.string.formulaKey),formula);
+        ArrayList<String> pastFormsArray = new ArrayList<>();
+
+        while(!pastFormulas.empty()){
+            pastFormsArray.add(pastFormulas.pop());
+        }
+        outState.putStringArrayList(getString(R.string.pastFormsKey),pastFormsArray);
+    }
+
+    /**
+     * This method is called after {@link #onStart} when the activity is
+     * being re-initialized from a previously saved state, given here in
+     * <var>savedInstanceState</var>.  Most implementations will simply use {@link #onCreate}
+     * to restore their state, but it is sometimes convenient to do it here
+     * after all of the initialization has been done or to allow subclasses to
+     * decide whether to use your default implementation.  The default
+     * implementation of this method performs a restore of any view state that
+     * had previously been frozen by {@link #onSaveInstanceState}.
+     *
+     * <p>This method is called between {@link #onStart} and
+     * {@link #onPostCreate}. This method is called only when recreating
+     * an activity; the method isn't invoked if {@link #onStart} is called for
+     * any other reason.</p>
+     *
+     * @param savedInstanceState the data most recently supplied in {@link #onSaveInstanceState}.
+     * @see #onCreate
+     * @see #onPostCreate
+     * @see #onResume
+     * @see #onSaveInstanceState
+     */
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ArrayList<String> pastFormsArray = savedInstanceState.getStringArrayList(getString(R.string.pastFormsKey));
+        for(int i = pastFormsArray.size()-1; i >= 0; i--){
+            pastFormulas.push(pastFormsArray.get(i));
+        }
+        formula = savedInstanceState.getString(getString(R.string.formulaKey));
+        updateDisplay();
     }
 }
